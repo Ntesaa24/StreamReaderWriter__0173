14d0aca95352c13a93e0a8d3d7c4c49f287e87b4
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-06-04 10:24:51 +0700

    loop

--- IoFile.cpp
+++ IoFile.cpp
@@ -15,4 +15,22 @@ int main()
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
-    
+    // unlimited loop untuk menulis
+    while (true)
+    {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        // menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    // selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    // membuka file dalam mode membaca
+    ifstream infile;
+    // menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+}
